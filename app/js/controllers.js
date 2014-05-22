6c9c415a0b88032d5887258b77df4d329df4c4f6
gustavodemari <guhdemari@gmail.com>
2014-05-22 02:09:07 -0300

    adding sectors view

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,26 @@
 'use strict';
 
 /* Controllers */
+var app = angular.module('precodomercado.controllers', []);
 
-angular.module('precodomercado.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  app.controller('MyCtrl1', ['$scope', function($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  }]);
 
+  app.controller('MyCtrl2', ['$scope', function($scope) {
+
+  }]);
+
+  app.controller('SectorsCtrl', ['$scope', function($scope) {
+    $scope.sectors = [
+      {
+        name: 'Sector1'
+      },
+      {
+        name: 'Sector2'
+      },
+      {
+        name: 'Sector3'
+      }
+    ];
   }]);
