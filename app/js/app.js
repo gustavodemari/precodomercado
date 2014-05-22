00e080d214061a8a4ac746afe65b1c9a44a2f437
gustavodemari <guhdemari@gmail.com>
2014-05-22 02:39:42 -0300

    updating industry view

--- app/js/app.js
+++ app/js/app.js
@@ -13,5 +13,6 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/sectors', {templateUrl: 'partials/sectors.html', controller: 'SectorsCtrl'});
+  $routeProvider.when('/industry/:industryId', {templateUrl: 'partials/industry.html', controller: 'IndustryCtrl'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -25,3 +25,18 @@ var app = angular.module('precodomercado.controllers', []);
     });
 
   }]);
+
+  app.controller('IndustryCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+    $scope.industry = [];
+
+    var YAHOO_API_URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.industry%20where%20id%3D%22'+$routeParams.industryId+'%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+
+    $http.get(YAHOO_API_URL)
+    .success(function(data){
+      console.log(data);
+      if(data.query.results){
+        $scope.industry = data.query.results.industry;
+      }
+    });
+
+  }]);
