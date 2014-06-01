63486bb779919ad9a979301ef79efab09fa21bd8
gustavodemari <guhdemari@gmail.com>
2014-05-31 21:39:59 -0300

    adding back button

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,16 +13,17 @@ var app = angular.module('precodomercado.controllers', []);
 
   app.controller('SectorsCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.sectors = [];
-
+    
     var YAHOO_API_URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.sectors&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
-
-    $http.get(YAHOO_API_URL)
-    .success(function(data){
-      if(data.query.results){
-        $scope.sectors = data.query.results.sector;
-      }
-    });
-
+    
+    if($scope.sectors.length === 0) {
+      $http.get(YAHOO_API_URL)
+        .success(function(data){
+          if(data.query.results){
+            $scope.sectors = data.query.results.sector;
+          }
+        });
+    }
   }]);
 
   app.controller('IndustryCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
